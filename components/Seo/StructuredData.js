1cb32c15b06e87e3bbce08e3418ff68e0cd7d512
Gust Pêtre <gpetre@metanous.be>
2026-02-23 18:06:37 +0100

    Hopelijk goede seo

--- components/Seo/StructuredData.js
+++ components/Seo/StructuredData.js
@@ -3,6 +3,7 @@ const SeoStructuredData = ({ type = "VacationRental", data }) => {
     "@context": "https://schema.org",
     "@type": type,
     "@id": "https://www.sanmarino4.be/#vacationrental",
+    identifier: "san-marino-4-middelkerke",
     name: "San Marino 4",
     description:
       "Luxe vakantiestudio in Middelkerke met zeezicht, geschikt voor 4 personen. Moderne inrichting met wifi, volledig uitgeruste keuken en parking.",
@@ -25,6 +26,16 @@ const SeoStructuredData = ({ type = "VacationRental", data }) => {
       "@type": "GeoCoordinates",
       latitude: "51.1852",
       longitude: "2.8217",
+    },
+    containedInPlace: {
+      "@type": "Place",
+      name: "Middelkerke",
+      address: {
+        "@type": "PostalAddress",
+        addressLocality: "Middelkerke",
+        addressRegion: "West-Vlaanderen",
+        addressCountry: "BE",
+      },
     },
   };
 
