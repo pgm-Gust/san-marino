4974f535b9af12eed04f08ac46ce0dda11ff81c1
Gust Pêtre <gpetre@metanous.be>
2026-08-04 22:42:46 +0100

    remove unused API routes and related components

--- app/api/testenv/route.js
+++
@@ -1,9 +0,0 @@
-export async function GET() {
-  return new Response(
-    JSON.stringify({
-      url: process.env.NEXT_PUBLIC_SUPABASE_URL,
-      key: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    }),
-    { headers: { "Content-Type": "application/json" } }
-  );
-}
